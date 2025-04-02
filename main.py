e8afa50d0747ae701999a8ad046b1748571ea4f4
LutsenkoVitaliy <lutsenko996@gmail.com>
2025-04-02 19:35:26 +0300

    cats_libary

--- main.py
+++ main.py
@@ -35,3 +35,33 @@ print(f"Загальна сума заробітної плати: {total}, Се
 
 # ДРУГЕ ЗАВДАННЯ
 path = '60b90c1c13067a15887e1ae1,Tayson,3\n60b90c2413067a15887e1ae2,Vika,1\n60b90c2e13067a15887e1ae3,Barsik,2\n60b90c3b13067a15887e1ae4,Simon,12\n60b90c4613067a15887e1ae5,Tessi,5'
+
+with open(parent_folder_path / 'cats_file.txt', 'w', encoding='utf-8') as cats_file:
+  cats_file.write(path)
+
+def get_cats_info(path):
+  with open(path, 'r', encoding='utf-8') as cats_file:
+    try:
+      cats_info_libary = []
+      lines = [el.strip() for el in cats_file.readlines()]
+
+      for el in lines:
+        id,name,age = el.split(',')
+        cats_info_libary.append[{'id': id, 'name': name, 'age': age}]
+      
+      print(cats_info_libary)
+      return cats_info_libary
+    
+    except FileNotFoundError:
+      print('1')
+      return []
+    except PermissionError:
+      print('2')
+      return []
+    except Exception as e:
+      print('3')
+      return []
+    
+
+cats_info = get_cats_info("./cats_file.txt")
+print(cats_info)
