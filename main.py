f9abedca8472dcb70e247e9e82ccaa5dbaec1a29
LutsenkoVitaliy <lutsenko996@gmail.com>
2025-04-02 19:26:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
 parent_folder_path = Path('./')
+
+#ПЕРШЕ ЗАВДАННЯ
 path = 'Alex Korp,3000\nNikita Borisenko,2000\nSitarama Raju,1000'
 
 with open (parent_folder_path / 'salary_file.txt', 'w', encoding='utf-8') as salary_file:
@@ -10,6 +12,15 @@ def total_salary(path):
   try: 
     with open(path, 'r', encoding='utf-8') as salary_file:
       salaries = []
+      lines = [el.strip() for el in salary_file.readlines()]
+
+      for el in lines:
+        name, salary = el.split(',')
+        salaries.append(int(salary))
+    
+    total = sum(salaries)
+    average = round(total / len(salaries))
+    return (total, average)
 
   except FileNotFoundError:
     return (0,0)
@@ -20,3 +31,7 @@ def total_salary(path):
   
 total, average = total_salary("./salary_file.txt")
 print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
+
+# ДРУГЕ ЗАВДАННЯ
+path = '60b90c1c13067a15887e1ae1,Tayson,3\n60b90c2413067a15887e1ae2,Vika,1\n60b90c2e13067a15887e1ae3,Barsik,2\n60b90c3b13067a15887e1ae4,Simon,12\n60b90c4613067a15887e1ae5,Tessi,5'
