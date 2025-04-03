e4d24f5b846550b753da39da79d098528d0ad8ea
LutsenkoVitaliy <lutsenko996@gmail.com>
2025-04-04 00:48:49 +0300

    Update hw03.py

--- hw03.py
+++ hw03.py
@@ -3,9 +3,16 @@ import os
 import sys
 from colorama import Fore, Back, Style
 
-print(Fore.RED + 'Це червоний текст')
-print(Back.GREEN + 'Це текст на зеленому фоні')
-print(Style.RESET_ALL)
-print('Це звичайний текст після скидання стилю')
+# print(Fore.RED + 'Це червоний текст')
+# print(Back.GREEN + 'Це текст на зеленому фоні')
+# print(Style.RESET_ALL)
+# print('Це звичайний текст після скидання стилю')
+
+folder_path = Path('./picture')
+
+def path_to_directory(path):
+  pass
 
 
+print(folder_path)
+# path_to_directory()
