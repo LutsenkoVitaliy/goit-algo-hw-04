500e62eec260e836cee1ed9547e01421728199dd
LutsenkoVitaliy <lutsenko996@gmail.com>
2025-04-03 23:07:29 +0300

    virtual environment

---
+++ hw03.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import os 
+import sys
+from colorama import Fore, Back, Style
+
+print(Fore.RED + 'Це червоний текст')
+print(Back.GREEN + 'Це текст на зеленому фоні')
+print(Style.RESET_ALL)
+print('Це звичайний текст після скидання стилю')
+
+
