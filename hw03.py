5c8cf7d6ca9aeae1cb9f5ad7aa62218085b86ccf
LutsenkoVitaliy <lutsenko996@gmail.com>
2025-04-05 20:53:56 +0300

    hw03 compete

--- hw03.py
+++ hw03.py
@@ -1,23 +1,30 @@
 from pathlib import Path
-import os 
 import sys
-from colorama import Fore, Back, Style
+from colorama import init,Fore, Back
 
-# print(Fore.RED + 'Це червоний текст')
-# print(Back.GREEN + 'Це текст на зеленому фоні')
-# print(Style.RESET_ALL)
-# print('Це звичайний текст після скидання стилю')
+init(autoreset=True)
 
 def folder_structure(directory, indent="  "):
-  pass
+  if not directory.exists() or not directory.is_dir():
+    print(Back.RED + "Вказана директорія не існує або не являється директорією")
+    return
+  
+  for element in directory.iterdir():
+    if element.is_dir():
+      print(Fore.BLUE + indent + f"{element.name}/")
+      folder_structure(element, indent + "  ")
+    
+    if element.is_file():
+      print(Fore.GREEN + indent + f"{element.name}")
+
 
 
 if __name__ == "__main__":
   if len(sys.argv) != 2:
-    print(f"Передані параметри не є дійсними, {" ".join(sys.argv)}")
-    print("Приклад: python hw03.py шлях/до/вашої/дирикторії")
+    print(Back.RED + f"Передані параметри не є дійсними: {' '.join(sys.argv)}")
+    print(Back.RED + "Приклад: python hw03.py шлях/до/вашої/директорії")
     sys.exit(1)
 
   path_to_directory = Path(sys.argv[1])
-  print(f"{path_to_directory}/")
+  print(Fore.BLUE + f"{path_to_directory}/")
   folder_structure(path_to_directory)
